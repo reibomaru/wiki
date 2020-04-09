59a16fa4daf67e5fde767baa3ec081d08987497e
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-04-10 00:01:35 +0900

    静的ファイルの表示に失敗

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-//wikiのデータ構造
+//Page wikiのデータ構造
 type Page struct {
 	Title string //タイトル
 	Body  []byte //タイトルの中身
@@ -27,13 +27,13 @@ var templates = make(map[string]*template.Template)
 var titleValidator = regexp.MustCompile("^[a-zA-Z0-9]+$")
 
 //.txt
-const expend_string = ".txt"
+const expendString = ".txt"
 
 //初期化関数
 func init() {
 	for _, tmpl := range []string{"edit", "view"} {
 		//エラーの場合Panicを起こすためエラー処理はなし
-		t := template.Must(template.ParseFiles(tmpl + ".html"))
+		t := template.Must(template.ParseFiles("template/" + tmpl + ".html"))
 		templates[tmpl] = t
 	}
 }
@@ -49,6 +49,15 @@ func getTitle(w http.ResponseWriter, r *http.Request) (title string, err error) 
 	return
 }
 
+// func newHandler(w http.ResponseWriter, r *http.Request) {
+// 	t := template.Must(template.ParseFiles("template/new.html"))
+// 	err = t.Execute(w)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+// }
+
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) { //*http.RequestではURLの情報にアクセスできる
 	p, err := loadPage(title)
 	if err != nil {
@@ -60,14 +69,16 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) { //*http
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
+	p, err := loadPage(title) //
 	if err != nil {
 		p = &Page{Title: title}
 	}
 	renderTemplate(w, "edit", p)
 }
 
+//
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	log.Print(r.FormValue("body"))
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -79,37 +90,44 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 }
 
 func topHandler(w http.ResponseWriter, r *http.Request) {
-	//main.goがいる階層のディレクトリにある.txtデータを取得する
+	//main.goがいる階層のディレクトリにあるfileを取得する
 	files, err := ioutil.ReadDir("./")
 	if err != nil {
 		err = errors.New("所定のディレクトリ内にテキストファイルがありません")
 		log.Print(err)
 		return
 	}
-
 	var paths []string    //テキストデータの名前
-	var fileName []string //でキスとデータのファイル名
+	var fileName []string //テキストデータのファイル名
+	// filesの中から.txtファイルの名前と
 	for _, file := range files {
 		//対象となる.txtデータのみを取得
-		if strings.HasSuffix(file.Name(), expend_string) {
+		if strings.HasSuffix(file.Name(), expendString) {
 			//テキストデータの .txtで名前をスライスしたものをfileNameに入れる
-			fileName = strings.Split(string(file.Name()), expend_string)
+			fileName = strings.Split(string(file.Name()), expendString)
+			// log.Println(fileName)
 			paths = append(paths, fileName[0])
 		}
 	}
+	// log.Println(paths)
 	//ファイルパスがなかった場合
 	if paths == nil {
 		err = errors.New("テキストファイルが存在しません")
 		log.Print(err)
 	}
 
-	t := template.Must(template.ParseFiles("top.html"))
+	t := template.Must(template.ParseFiles("template/top.html"))
 	err = t.Execute(w, paths)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
+
+// func newHandler(w http.ResponseWriter, r *http.Request) {
+// 	renderTemplate(w, "/new/", nil)
+// }
+
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		//Requestからページタイトルを取り出して、fnを呼び出す
@@ -153,9 +171,12 @@ func loadPage(title string) (*Page, error) {
 }
 
 func main() {
+	fs := http.FileServer(http.Dir("/"))
+	// log.Print(fs)
+	http.Handle("/new/", fs)
 	http.HandleFunc("/view/", makeHandler(viewHandler)) //パスを指定してどういった動きにするのかをハンドリングする
 	http.HandleFunc("/edit/", makeHandler(editHandler)) //パスを指定してどういった動きにするのかをハンドリングする
 	http.HandleFunc("/save/", makeHandler(saveHandler)) //パスを指定してどういった動きにするのかをハンドリングする
 	http.HandleFunc("/top/", topHandler)                //パスを指定してどういった動きにするのかをハンドリングする
-	http.ListenAndServe(":8080", nil)                   //サーバーを自分のPCの中で立ち上げている、ポートを8080としてたちあげる
+	http.ListenAndServe(":3000", nil)                   //サーバーを自分のPCの中で立ち上げている、ポートを8080としてたちあげる
 }
